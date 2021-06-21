cfe1ff06126e43b3b8889b08b8d5c9dcc24916f3
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2021-06-21 10:42:03 -0400

    add data-react to meta tags

--- react-lights-out-starter/src/App.js
+++ react-lights-out-starter/src/App.js
@@ -13,13 +13,14 @@ class App extends Component {
       <div className='App'>
       <Helmet>
           <meta charset="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta name="theme-color" content="#000000" />
-          <meta name="image" property="og:image" content="https://image.prntscr.com/image/yiujgjgzSHqyxvP8UAlb1w.png" />
-          <meta name="url" property="og:url" content="https://zen-bhaskara-0d6cd4.netlify.app/" />
-          <meta name="description" property="og:description" content="LightsOut | Can YOU turn the lights out?" />
-          <meta name="author" property="og:author" content="Michael Cooper" />
-          <meta name="publish_date" property="og:publish_date" content="2021-06-18T00:00:00-0600" />
+          <meta name="title" property="og:title" content="LightsOut" data-react-helmet="true" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" data-react-helmet="true" />
+          <meta name="theme-color" content="#000000" data-react-helmet="true" />
+          <meta name="image" property="og:image" content="https://image.prntscr.com/image/yiujgjgzSHqyxvP8UAlb1w.png" data-react-helmet="true" />
+          <meta name="url" property="og:url" content="https://zen-bhaskara-0d6cd4.netlify.app/" data-react-helmet="true" />
+          <meta name="description" property="og:description" content="LightsOut | Can YOU turn the lights out?" data-react-helmet="true" />
+          <meta name="author" property="og:author" content="Michael Cooper" data-react-helmet="true" />
+          <meta name="publish_date" property="og:publish_date" content="2021-06-18T00:00:00-0600" data-react-helmet="true" />
           <script src="https://kit.fontawesome.com/8cc1b75eb3.js" crossorigin="anonymous"></script>
           <title>LightsOut</title>
       </Helmet>
