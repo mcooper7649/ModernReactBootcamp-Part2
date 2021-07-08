95828340eb8bd125e9ec06d936ddb08c2719a5bf
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2021-07-08 12:44:25 -0400

    Complete React Bootstrap configuration and menubar

---
+++ dog-list/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import './App.css';
+import { Nav, Navbar, NavDropdown, Form, FormControl, Button} from 'react-bootstrap'
+import DogList from './DogList.js'
+
+function App() {
+  return (
+    <div className="App">
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#link">Link</Nav.Link>
+      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-success">Search</Button>
+    </Form>
+  </Navbar.Collapse>
+</Navbar>
+    <h1 className="display-1">Dog App</h1>
+      <DogList />
+    </div>
+  );
+}
+
+export default App;
---
+++ dog-list/src/DogList.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+
+class DogList extends Component {
+
+    // static defaultProps = {
+    //     dogs: [
+    //       {
+    //         name: "Whiskey",
+    //         age: 5,
+    //         src: whiskey,
+    //         facts: [
+    //           "Whiskey loves eating popcorn.",
+    //           "Whiskey is a terrible guard dog.",
+    //           "Whiskey wants to cuddle with you!"
+    //         ]
+    //       },
+    //       {
+    //         name: "Hazel",
+    //         age: 3,
+    //         src: hazel,
+    //         facts: [
+    //           "Hazel has soooo much energy!",
+    //           "Hazel is highly intelligent.",
+    //           "Hazel loves people more than dogs."
+    //         ]
+    //       },
+    //       {
+    //         name: "Tubby",
+    //         age: 4,
+    //         src: tubby,
+    //         facts: [
+    //           "Tubby is not the brightest dog",
+    //           "Tubby does not like walks or exercise.",
+    //           "Tubby loves eating food."
+    //         ]
+    //       }
+    //     ]
+    //   }
+
+constructor(props){
+    super(props);
+}
+    render(){
+        return (
+        <div className="DogList">
+            <h1>test</h1>
+        </div>
+        )  
+    }
+}
+
+export default DogList;
